39b033cabc762cfb7113d2449198d51df4026106
Matheus Pacheco <matheus_pachecosilva@hotmail.com>
2025-05-20 06:52:04 -0300

    Ajustando localização para pt-br

--- cadastro-livros-frontend/src/app/app.module.ts
+++ cadastro-livros-frontend/src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutModule } from './layout/layout.module';
+
+// Importações necessárias para pt-BR
+import { registerLocaleData } from '@angular/common';
+import ptBr from '@angular/common/locales/pt';
+registerLocaleData(ptBr);
 
 @NgModule({
   declarations: [AppComponent],
@@ -16,7 +21,7 @@ import { LayoutModule } from './layout/layout.module';
     LayoutModule,
     HttpClientModule,
   ],
-  providers: [],
+  providers: [{ provide: LOCALE_ID, useValue: 'pt-BR' }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
